de5fadf87788f0c7efaaef8d9bb2a0cc3c3640ca
Pedro <predoisac@gmail.com>
2025-09-08 20:38:18 -0300

    api iniciozinho

---
+++ src/api/characters.jsx
@@ -0,0 +1,12 @@
+import api from "./api"
+
+
+export const GetCharacters = async () => {
+    const response = await api.get("/characters")
+
+    if (response.status != 200) {
+        return []
+    }
+
+    return response.data
+}
